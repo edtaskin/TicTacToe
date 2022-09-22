faddc9fc8ff7f245de383258670d3c6c0e1b63a1
edtaskin <91011207+edtaskin@users.noreply.github.com>
2022-09-22 15:48:20 +0300

    Added title to the stage

--- TicTacToe/src/application/Main.java
+++ TicTacToe/src/application/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			stage.setScene(scene);
+			stage.setTitle("TicTacToe");
 			stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
